36f6441dd68c5095e36847b82ac0b2bd7fa5108e
Sandeep Kumar <sandeepkrdhiman05@gmail.com>
2024-02-09 23:09:36 +0530

    first push for backend

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,44 @@
+// ========== REGISTER A NEW USER
+// POST api/users/register
+// UNPROTECTED
+const registerUser =(req, res, next) => {
+  res.json("Register User")
+}
+
+// ========== LOGIN A REGISTER USER
+// POST api/users/login
+// PROTECTED
+const loginUser =(req, res, next) => {
+  res.json("Login User")
+}
+
+// ========== USER PROFILE
+// POST api/users/:id
+// PROTECTED
+const getUser =(req, res, next) => {
+  res.json("Get User")
+}
+
+
+// ========== CHANGE USER AVATAR (profile picture)
+// POST api/users/change-avatar
+// PROTECTED
+const changeAvatar =(req, res, next) => {
+  res.json("Change User Avatar")
+}
+
+// ========== EDIT USER DETAILS (from profile)
+// POST api/users/edit-user
+// PROTECTED
+const editUser =(req, res, next) => {
+  res.json("Edit User Details")
+}
+
+// ========== USER AUTHORS
+// POST api/users/authors
+// UNPROTECTED
+const getAuthors =(req, res, next) => {
+  res.json("Get all users/authors")
+}
+
+module.exports = {registerUser, loginUser, getUser, changeAvatar, editUser, editUser, getAuthors }
---
+++ server/routes/postRoutes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', (req, res, next) => {
+  res.json({ message: 'This is the posts router' })
+});
+
+module.exports = router;
