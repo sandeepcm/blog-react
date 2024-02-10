a409835a409a716611c58cbd5bcc7796085242b1
sandeepcm <sandeepkrdhiman05@gmail.com>
2024-02-10 22:39:41 +0530

    chore(): avtar file update

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken')
-const HttpError = require('../models/errorModel')
+const jwt = require('jsonwebtoken');
+const HttpError = require('../models/errorModel');
 
-const authMiddleware = async(req, res, next) => {
-  const Authorization = req.headers.Authorization || req.headers.authorization
+const authMiddleware = async (req, res, next) => {
+  const authorization = req.headers.authorization;
 
-  if(Authorization && Authorization.startWith("Bearer")) {
-    const token = Authorization.split(' ')[1]
+  if (authorization && authorization.startsWith("Bearer")) {
+    const token = authorization.split(' ')[1];
     jwt.verify(token, process.env.JWT_SECRET, (err, info) => {
-      if(err) {
-        return next(new HttpError("unauthorized. Invalid tokeneeeeeeee.", 403))
+      if (err) {
+        return next(new HttpError("Unauthorized. Invalid token.", 403));
       }
-      req.user = info
-      next()
-    })
-  } else{
-    return next(new HttpError("unauthorized. Invalid tokensssssssssss.", 402))
+      req.user = info;
+      next();
+    });
+  } else {
+    return next(new HttpError("Unauthorized. Invalid token.", 402));
   }
-}
+};
 
-module.exports = authMiddleware
+module.exports = authMiddleware;
